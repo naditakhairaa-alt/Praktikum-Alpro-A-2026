d771235b23f2add4838c270abbe67f8e6466d98b
naditakhairaa-alt <naditakhairaa@gmail.com>
2026-04-28 16:24:32 +0700

    Pertemuan 3

--- Pertemuan 3/latihan.py
+++ Pertemuan 3/latihan.py
@@ -1,14 +1,14 @@
 class Jam:
     def __init__(self, bentuk, merk, bahan):
-     self.bentuk = bentuk
-     self.merk = merk
-     self.bahan = bahan
-
-def printjam(self):
-    print(self.bentuk)
-
-def ubahJam(self, bahanBagus):
-    self.bahan = bahanBagus
+       self.bentuk = bentuk
+       self.merk = merk
+       self.bahan = bahan
+       
+    def printjam(self):
+        print(self.bentuk)
+    
+    def ubahJam(self, bahanBagus):
+        self.bahan = bahanBagus
 
 p1 = Jam("Bulat", "Casio", "Kulit")
 p2 = Jam("Persegi", "AC", "Karet")
@@ -17,3 +17,4 @@ p3 = Jam("Oval", "Rolex", "Rantai")
 print (p1.merk)
 print (p2.merk)
 print (p3.merk)
+p2.printjam()
